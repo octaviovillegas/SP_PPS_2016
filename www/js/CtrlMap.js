2e2a48074a59fd9d6e2a8a23ae9d7323167f564b
AcostaDemian <acostademianariel@gmail.com>
2016-11-11 22:02:38 -0300

    Floating menue en el mapa, se puede enviar SOS, falta cambiar iconos de los floating buttons

--- www/js/CtrlMap.js
+++ www/js/CtrlMap.js
@@ -1,6 +1,8 @@
 angular.module('starter')
 
-.controller('mapaCtrl', function($scope, $stateParams,$firebaseArray,$timeout,Info) {
+.controller('mapaCtrl', function($scope, $stateParams,$firebaseArray,$timeout,Info, $ionicPopup,$filter) {
+
+  $scope.marcasMapa =Info;
 
   var options = {
                   enableHighAccuracy: true
@@ -16,7 +18,53 @@ angular.module('starter')
                           alert('Unable to get location: ' + error.message);
                       }, options);
 
-  $scope.marcasMapa =Info;
+
+  $scope.infos = Info;
+   
+    // When button is clicked, the popup will be shown...
+   $scope.showPopup = function(tipo) {
+      $scope.data = {}
+    
+      // Custom popup
+      var myPopup = $ionicPopup.show({
+         template: 'Seguro que queres mandar un SOS?',
+         title: 'Enviar SOS',
+         scope: $scope,
+      
+         buttons: [
+            { text: 'Cancel' }, 
+            {text: '<b>Save</b>',
+             type: 'button-positive',
+                  onTap: function(e) {
+                    var options = {
+                        enableHighAccuracy: true
+                    };
+                    var latlon;
+                      navigator.geolocation.getCurrentPosition(function(pos) {
+                         $scope.infos.$add({
+                           "tipo": tipo,
+                            "lat":pos.coords.latitude,
+                            "lon": pos.coords.longitude,
+                            "usu":"pepe",
+                            "date": $filter('date')(new Date(), 'dd/MM/yyyy')
+                          });               
+                      }, 
+                      function(error) {                    
+                          alert('Unable to get location: ' + error.message);
+                      }, options);
+
+                          
+                           
+                  }
+            }
+         ]
+      });
+
+      myPopup.then(function(res) {
+        
+      });    
+   };
+
 });
 
 
