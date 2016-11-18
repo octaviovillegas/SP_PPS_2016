f867c51897535b7b982e23b2250058c7efcc195d
AcostaDemian <acostademianariel@gmail.com>
2016-11-18 20:53:41 -0300

    ..

--- www/js/listaDeUsuariosCtrl.js
+++ www/js/listaDeUsuariosCtrl.js
@@ -4,6 +4,19 @@ angular.module('starter')
 
   $scope.info =Info;
 
+  var options = {
+                  enableHighAccuracy: true
+                };
+
+  navigator.geolocation.getCurrentPosition(function(pos) {
+                         $scope.inicial=({
+                            "lat":pos.coords.latitude,
+                            "lon": pos.coords.longitude
+                          });               
+                      },
+                      function(error) {                    
+                          alert('Unable to get location: ' + error.message);
+                      }, options);
 
 });
 
